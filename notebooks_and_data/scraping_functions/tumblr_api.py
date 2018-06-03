f69869d3d4b8b8a70f301c628761abefeb64ae0c
Victoria Ng <victoria.ng555@gmail.com>
2018-06-03 12:22:05 -0500

    Add tumblr user function

--- notebooks_and_data/scraping_functions/tumblr_api.py
+++ notebooks_and_data/scraping_functions/tumblr_api.py
@@ -14,3 +14,27 @@ def get_client():
     )
 
     return client
+
+def get_tumblr_usernames_for_hashtags(list_of_hashtags):
+    usernames = set()
+    for hashtag in list_of_hashtags:
+        os.environ['webdriver.chrome.driver'] = 'chromedriver'
+        driver = webdriver.Chrome()
+        driver.get('https://www.tumblr.com/search/{}'.format(hashtag[1:]))
+        usernames = usernames.union(get_tumblr_usernames(driver))
+    return usernames
+
+def extract_tumblr_usernames_from_page(driver):
+    """
+    Get a list of all the usernames from the search results page.
+    """
+
+    try:
+        username_elements = driver.find_elements_by_class_name('post-info-tumblelog')
+        usernames = [element.text for element in username_elements]
+    except:
+        # default to a single username
+        usernames = set(['greasyquotes'])
+
+    return usernames
+
